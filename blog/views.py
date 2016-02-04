b2bb14a77449ca47a2be43b616ca4cbe078e05c1
alex <alexander.dahl93@gmail.com>
2016-02-04 22:09:44 +0100

    following djanog guide

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("hej världen")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/index.html', context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'blog/detail.html', {'question': question})
 
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
 # Create your views here.
