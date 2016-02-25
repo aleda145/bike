dd83b4969a4a4f48fd742b40ed6f8b290f2804cf
alex <alexander.dahl93@gmail.com>
2016-02-25 22:10:06 +0100

    working blog application, ugly as sin

--- bike/urls.py
+++ bike/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     url(r'^blog/', include ('blog.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+
 
 ]
--- blog/models.py
+++ blog/models.py
@@ -4,12 +4,13 @@ from django.db import models
 from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    body = models.TextField()
 
     def __str__(self):
-        return self.question_text
+        return self.title
 
 ##demonstration
     def was_published_recently(self):
@@ -18,10 +19,4 @@ class Question(models.Model):
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.choice_text
